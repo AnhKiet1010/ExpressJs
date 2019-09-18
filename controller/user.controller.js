2b24471e19f69c5873f06123f96843c1f2745d55
TieKie <letrananhkiet@gmail.com>
2019-09-18 13:19:31 +0900

    validate user login

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -19,10 +19,25 @@ module.exports.search = function(req,res) {
 
 module.exports.postCreate = function(req,res) {
 	req.body.id = shortid.generate();
-	db.get('users')
-  	.push(req.body)
-  	.write();
-  	res.redirect('/users');
+	var errors = [];
+	if(!req.body.name) {
+		errors.push('user name is riqured.');
+	}
+
+	if(!req.body.phone) {
+		errors.push('phone is required.')
+	}
+
+	if(errors.length) {
+		res.render('users/create', {
+			errors: errors,
+			values: req.body
+		});
+		return;
+	}
+
+	db.get('users').push(req.body).write();
+	res.redirect('/users');
 };
 
 module.exports.create = function(req,res) {
